83848b556b42850fafb6c5d7865a5f7f0805ba72
tiansnowfly <1737145412@qq.com>
2021-09-04 20:02:03 +0800

    update charts

--- static/js/anomalyAnalysis.js
+++ static/js/anomalyAnalysis.js
@@ -12,9 +12,9 @@ function abnormalAnalysisView() {
                 var curUser=Object.keys(data)[0];
                 var curUserWC=data[curUser][1];
                 var curUserLine=data[curUser][2];
-                createFilterTable(table);
-                createCurrentUserWordCloud(curUserWC);
-                createCurrentUserLine(curUserLine);
+                // createFilterTable(table);
+                // createCurrentUserWordCloud(curUserWC);
+                // createCurrentUserLine(curUserLine);
             },
             error:function (e) {
                 alert("Anomaly: "+e);
@@ -135,5 +135,195 @@ function abnormalAnalysisView() {
 
         lOption && chart.setOption(lOption);
     }
+
+    function drawUserKeyWords() {
+        var chart=echarts.init(document.getElementById("user-word-cloud"));
+
+        option = {
+            legend: {
+                top: 'bottom'
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: {show: true},
+                    dataView: {show: true, readOnly: false},
+                    restore: {show: true},
+                    saveAsImage: {show: true}
+                }
+            },
+            series: [
+                {
+                    name: '面积模式',
+                    type: 'pie',
+                    radius: [20, 80],
+                    center: ['50%', '50%'],
+                    roseType: 'area',
+                    itemStyle: {
+                        borderRadius: 8
+                    },
+                    data: [
+                        {value: 40, name: '治疗'},
+                        {value: 38, name: '护理'},
+                        {value: 32, name: '应用'},
+                        {value: 30, name: '诊断'},
+                        {value: 28, name: '疾病'},
+                        {value: 26, name: '大数据'},
+                        {value: 22, name: 'AI'},
+                        {value: 18, name: '综述'},
+                        {valye: 16, name: '其他'}
+                    ]
+                }
+            ]
+        };
+        option && chart.setOption(option);
+    }
+    drawUserKeyWords();
+    
+    function drawTopUserArticle() {
+        var ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
+
+        var chartDom = document.getElementById('ab-user-line');
+        var myChart = echarts.init(chartDom);
+        var option;
+
+        myChart.showLoading();
+        $.get(ROOT_PATH + '/data/asset/data/flare.json', function (data) {
+            myChart.hideLoading();
+
+            myChart.setOption(option = {
+                tooltip: {
+                    trigger: 'item',
+                    triggerOn: 'mousemove'
+                },
+                series:[
+                    {
+                        type: 'tree',
+
+                        data: [data],
+
+                        left: '2%',
+                        right: '2%',
+                        top: '8%',
+                        bottom: '20%',
+
+                        symbol: 'emptyCircle',
+
+                        orient: 'vertical',
+
+                        expandAndCollapse: true,
+
+                        label: {
+                            position: 'top',
+                            rotate: -90,
+                            verticalAlign: 'middle',
+                            align: 'right',
+                            fontSize: 9
+                        },
+
+                        leaves: {
+                            label: {
+                                position: 'bottom',
+                                rotate: -90,
+                                verticalAlign: 'middle',
+                                align: 'left'
+                            }
+                        },
+
+                        animationDurationUpdate: 750
+                    }
+                ]
+            });
+        });
+
+        option && myChart.setOption(option);
+    }
+    drawTopUserArticle();
+
+    function drawUserLine() {
+        var ROOT_PATH = 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/examples';
+
+        var chartDom = document.getElementById('user-article-tree');
+        var myChart = echarts.init(chartDom);
+        var option;
+
+        $.get(ROOT_PATH + '/data/asset/data/life-expectancy-table.json', function (_rawData) {
+            run(_rawData);
+        });
+
+        function run(_rawData) {
+
+            option = {
+                dataset: [{
+                    id: 'dataset_raw',
+                    source: _rawData
+                }, {
+                    id: 'dataset_since_1950_of_germany',
+                    fromDatasetId: 'dataset_raw',
+                    transform: {
+                        type: 'filter',
+                        config: {
+                            and: [
+                                { dimension: 'Year', gte: 1950 },
+                                { dimension: 'Country', '=': 'Germany' }
+                            ]
+                        }
+                    }
+                }, {
+                    id: 'dataset_since_1950_of_france',
+                    fromDatasetId: 'dataset_raw',
+                    transform: {
+                        type: 'filter',
+                        config: {
+                            and: [
+                                { dimension: 'Year', gte: 1950 },
+                                { dimension: 'Country', '=': 'France' }
+                            ]
+                        }
+                    }
+                }],
+                title: {
+                    text: '用户下载浏览记录折线图'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                xAxis: {
+                    type: 'category',
+                    nameLocation: 'middle'
+                },
+                yAxis: {
+                    name: 'Income'
+                },
+                series: [{
+                    type: 'line',
+                    datasetId: 'dataset_since_1950_of_germany',
+                    showSymbol: false,
+                    encode: {
+                        x: 'Year',
+                        y: 'Income',
+                        itemName: 'Year',
+                        tooltip: ['Income'],
+                    }
+                }, {
+                    type: 'line',
+                    datasetId: 'dataset_since_1950_of_france',
+                    showSymbol: false,
+                    encode: {
+                        x: 'Year',
+                        y: 'Income',
+                        itemName: 'Year',
+                        tooltip: ['Income'],
+                    }
+                }]
+            };
+
+            myChart.setOption(option);
+
+        }
+
+        option && myChart.setOption(option);
+    }
+    drawUserLine();
 }
 abnormalAnalysisView();
--- static/js/wordCloud.js
+++ static/js/wordCloud.js
@@ -10,12 +10,45 @@ function setWordCloud(type,month) {
         dataType: "JSON",
         success: function (keywords) {
             console.log(keywords);
+            keywords={
+                "护理": 1129,
+                "治疗": 1041,
+                "应用": 976,
+                "对策": 882,
+                "诊断": 861,
+                "儿童": 818,
+                "综述": 712,
+                "影响因素": 610,
+                "问题": 575,
+                "预后": 574,
+                "大型底栖动物": 494,
+                "现状": 484,
+                "人工智能": 478,
+                "危险因素": 472,
+                "大数据": 459,
+                "指南": 430,
+                "磁共振成像": 419,
+                "并发症": 418,
+                "管理": 409,
+                "糖尿病": 406,
+                "设计": 386,
+                "疗效": 378,
+                "研究进展": 363,
+                "发展": 356,
+                "策略": 352,
+                "分析": 350,
+                "影响": 336,
+                "大学生": 334,
+                "老年人": 328,
+                "生活质量": 324
+            };
             let wcData=Object.keys(keywords).map(function (item,index) {
                 return {name:item,value:Math.sqrt(keywords[item])};
             });
             let chart_word_cloud = echarts.init(document.getElementById('word-cloud'));
             let maskImage = new Image();
             let option_word_cloud = {
+
                 series: [ {
                     type: 'wordCloud',
                     sizeRange: [5, 30],
@@ -55,4 +88,4 @@ function setWordCloud(type,month) {
         }
     });
 }
-// setWordCloud(0,4);
+setWordCloud(0,4);
